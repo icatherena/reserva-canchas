3f945ce97b0d9cee081c97e3dcd31c64b0d30cc9
MartinDanielB <b.martindaniel@gmail.com>
2022-12-14 09:59:57 -0300

    inicializacion spring

---
+++ canchas/src/test/java/reserva/canchas/canchas/CanchasApplicationTests.java
@@ -0,0 +1,13 @@
+package reserva.canchas.canchas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanchasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
